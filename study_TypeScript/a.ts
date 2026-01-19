cc8107899d8970be63bc85331dbf249181c8a916
duanmengmeng <dcdmmcomeon@gmail.com>
2026-01-19 11:32:37 +0800

    最近没时间了哦

---
+++ study_TypeScript/a.ts
@@ -0,0 +1,12 @@
+let a: string = "hello"
+let b: number = 123
+let c: boolean = true
+let d: any = { name: "TypeScript" }
+let e: string[] = ["a", "b", "c"]
+let f: [string, number] = ["age", 30]
+
+console.log(a, b, c, d, e, f)
+
+
+let person: { name: string; age: number } = { name: "Alice", age: 25 }
+console.log(person)
