7ddacb1cff4c569317494c8aa37bd7dca6fed8dd
duanmengmeng <dcdmmcomeon@gmail.com>
2026-01-20 17:39:02 +0800

    类简单学习

---
+++ study_TypeScript/b_class0.ts
@@ -0,0 +1,70 @@
+class Point {
+    x: number;
+    y: number;
+
+    // 构造函数
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // 方法
+    print(): void {
+        console.log(`(${this.x}, ${this.y})`);
+    }
+
+    scale(n: number): void {
+        this.x *= n;
+        this.y *= n;
+    }
+
+    static x = 0; // 静态属性
+
+    // 静态方法
+    static printX() {
+        console.log(Point.x);
+    }
+}
+
+const pt = new Point();
+pt.print();
+pt.x = 10;
+pt.print();
+pt.scale(2);
+pt.print();
+// 可以通过类名直接访问静态属性和方法
+console.log(Point.x);
+Point.printX();
+
+class Animal {
+    move() {
+        console.log("Moving along!");
+    }
+
+    print() {
+        console.log("Hello, world!");
+    }
+}
+
+class Dog extends Animal {
+    woof(times: number) {
+        for (let i = 0; i < times; i++) {
+            console.log("woof!");
+        }
+    }
+
+    // 方法重写
+    print(name?: string) {
+        if (name === undefined) {
+            super.print();
+        } else {
+            console.log(`Hello, ${name.toUpperCase()}`);
+        }
+    }
+}
+
+const d = new Dog();
+d.move();
+d.woof(2);
+d.print();
+d.print("reader");
--- study_TypeScript/b_function1.ts
+++ study_TypeScript/b_function1.ts
@@ -1,34 +1,37 @@
+type AddFunction = (a: number) => number;
+type AddFunction2 = (a: number, b: number) => number;
+
 // Traditional anonymous function
-(function (a: any): any {
+let x0: AddFunction = (function (a: number): number {
     return a + 100;
 });
 
 // 1. Remove the word "function" and place arrow between the argument and opening body brace
-(a) => {
+let x1: AddFunction = (a) => {
     return a + 100;
 }; // 箭头函数(箭头函数总是匿名的)
 
 // 2. Remove the body braces and word "return" — the return is implied.
-(a) => a + 100;
+let x2: AddFunction = (a) => a + 100;
 
 // 3. Remove the parameter parentheses
-a => a + 100;
+let x3: AddFunction = a => a + 100;
 
-(function (a, b) {
+let y0: AddFunction2 = (function (a, b) {
     return a + b + 100;
 });
 
 // The parentheses can only be omitted if the function has a single simple parameter.
-(a, b) => a + b + 100;
+let y1: AddFunction2 = (a, b) => a + b + 100;
 
 
-(function (a, b) {
+let z0: AddFunction2 = (function (a, b) {
     const chuck = 42;
     return a + b + chuck;
 });
 
 // The braces can only be omitted if the function directly returns an expression. If the body has statements, the braces are required — and so is the return keyword.
-(a, b) => {
+let z1: AddFunction2 = (a, b) => {
     const chuck = 42;
     return a + b + chuck;
 };
