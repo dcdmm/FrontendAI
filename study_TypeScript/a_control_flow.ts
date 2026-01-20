4a4494237e83b9b31c6ecf5d995e356b7e4e9db4
duanmengmeng <dcdmmcomeon@gmail.com>
2026-01-20 10:49:39 +0800

    控制流

---
+++ study_TypeScript/a_control_flow.ts
@@ -0,0 +1,25 @@
+let score: number = 85;
+// if...else statement
+if (score >= 90) {
+    console.log('优秀');
+} else if (score >= 60) {
+    console.log('及格');
+} else {
+    console.log('不及格');
+}
+
+let day: number = 3;
+// switch statement
+switch (day) {
+    case 1:
+        console.log('星期一');
+        break;
+    case 2:
+        console.log('星期二');
+        break;
+    case 3:
+        console.log('星期三');
+        break;
+    default:
+        console.log('其他');
+}
