25a16b9a7eda9c324b16e1db411e21e161659ac3
duanmengmeng <dcdmmcomeon@gmail.com>
2026-01-20 15:12:58 +0800

    错误处理

---
+++ study_TypeScript/a_error_handing.ts
@@ -0,0 +1,13 @@
+function divide(a: number, b: number): number {
+    if (b === 0) {
+        throw new Error('不能除以0'); // Use the throw statement to throw an exception.
+    }
+    return a / b;
+}
+
+try {
+    const result = divide(10, 0);
+    console.log(result);
+} catch (error) {
+    console.log('出错了:', error.message);
+}
