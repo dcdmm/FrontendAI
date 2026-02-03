aec572057c5204848c2a8a6bb080689a51c35dc6
duanmengmeng <dcdmmcomeon@gmail.com>
2026-02-03 14:27:26 +0800

    持续完善

--- study_TypeScript/declare-module/main.ts
+++ study_TypeScript/declare-module/main.ts
@@ -2,9 +2,13 @@ import {greet, add} from 'my-custom'
 
 console.log(greet('TypeScript'))
 console.log(add(1, 2))
+// 编译器报错:TS2345: Argument of type string is not assignable to parameter of type numbe
+// console.log(add("1", 2) )
 
 import data from './test.json'
 
 console.log(data.name)
 console.log(data.version)
 console.log(data.tags)
+// 编译器报错:TS2339: Property age does not exist on type MyDataContent
+// console.log(data.age)
--- study_TypeScript/declare-module/types.d.ts
+++ study_TypeScript/declare-module/types.d.ts
@@ -1,9 +1,17 @@
+// 为"my-custom"模块提供类型声明(假装存在,避免导入时编译报错,并获得类型检查)
 declare module 'my-custom' {
     export function greet(name: string): string
 
+    /**
+     * 两数相加并返回结果
+     * @param a 加数
+     * @param b 加数
+     * @returns 相加结果
+     */
     export function add(a: number, b: number): number
 }
 
+// 为所有`.json`文件的导入提供类型声明(`*`表示匹配任意文件名)
 declare module '*.json' {
     interface MyDataContent {
         name: string
@@ -12,6 +20,6 @@ declare module '*.json' {
     }
 
     const content: MyDataContent
-    export default content
+    export default content // 默认导出
 }
 
