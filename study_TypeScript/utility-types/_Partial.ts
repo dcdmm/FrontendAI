45519d228dac609ba96f2d1f081160eeffb4ec03
duanmengmeng <dcdmmcomeon@gmail.com>
2026-02-04 11:30:28 +0800

    add utility function to update Todo items with partial fields

---
+++ study_TypeScript/utility-types/_Partial.ts
@@ -0,0 +1,23 @@
+// Constructs a type with all properties of Type set to optional. This utility will return a type that represents all subsets of a given type.
+
+interface Todo {
+    title: string;
+    description: string;
+}
+
+function updateTodo(todo: Todo, fieldsToUpdate: Partial<Todo>) {
+    console.log(fieldsToUpdate) // print->{ description: 'throw out trash' }
+    return {...todo, ...fieldsToUpdate};
+}
+
+const todo1 = {
+    title: "organize desk",
+    description: "clear clutter",
+};
+
+const todo2 = updateTodo(todo1, {
+    description: "throw out trash",
+});
+
+console.log(todo1); // print->{ title: 'organize desk', description: 'clear clutter' }
+console.log(todo2); // print->{ title: 'organize desk', description: 'throw out trash' }
