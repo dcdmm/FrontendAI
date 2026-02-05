a7da86adf907ae2addf843b10b23575ab96f652e
duanmengmeng <dcdmmcomeon@gmail.com>
2026-02-05 17:39:59 +0800

    refactor: 更新eslint配置，添加react-refresh插件并优化类型声明

--- demo0/frontend/eslint.config.js
+++ demo0/frontend/eslint.config.js
@@ -1,18 +1,23 @@
-import eslint from '@eslint/js'
-import { defineConfig } from 'eslint/config'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js'
+import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig(
-    { ignores: ['dist'] },
-    eslint.configs.recommended,
-    tseslint.configs.recommended,
+export default defineConfig([
+    globalIgnores(['dist']),
     {
-        plugins: {
-            'react-hooks': reactHooks,
-        },
-        rules: {
-            ...reactHooks.configs.recommended.rules,
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite,
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
         },
     },
-)
+])
--- demo0/frontend/src/vite-env.d.ts
+++ demo0/frontend/src/vite-env.d.ts
@@ -1,5 +1,12 @@
-/// <reference types="vite/client" />
-
 interface ImportMetaEnv {
     readonly VITE_API_URL?: string
-}
+} // 同名接口(node_modules\vite\types\importMeta.d.ts)自动合并
+
+/**
+ * The type of `import.meta`.
+ *
+ * If you need to declare that a given property exists on `import.meta`,
+ * this type may be augmented via interface merging.
+ */
+// interface ImportMeta {
+// }
