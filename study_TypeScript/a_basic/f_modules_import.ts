742d37e5b35466daf9dad23f3e5cdf913f8ec778
duanmengmeng <dcdmmcomeon@gmail.com>
2026-02-06 17:25:00 +0800

    feat: 添加泛型函数、接口和类的示例，展示TypeScript的类型系统

--- study_TypeScript/a_basic/f_modules_import.ts
+++ study_TypeScript/a_basic/f_modules_import.ts
@@ -1,20 +1,20 @@
 import { count } from 'node:console';
-import {add, PI, subtract, User, Calculator, VERSION, AUTHOR, multiply} from './e_modules_export';
+import {add, PI, subtract, User, Calculator, VERSION, AUTHOR, multiply} from './f_modules_export';
 // 重命名导入
-import {divide as divide_as} from './e_modules_export';
+import {divide as divide_as} from './f_modules_export';
 
 let result_d = divide_as(10, 2);
 console.log(`10 / 2 = ${result_d}`);
 
 // 所有导出的内容放在eme对象里(避免命名冲突)
-import * as eme from './e_modules_export';
+import * as eme from './f_modules_export';
 let add_r = eme.add(5, 7);
 let sub_r = eme.subtract(10, 3);
 let div_r = eme.divide(20, 4);
 console.log(sub_r, add_r, div_r, eme.PI);
 
 // export default(不用添加花括号,名称可自定义)
-import hello_x from './e_modules_export';
+import hello_x from './f_modules_export';
 console.log(hello_x("TypeScript"));
 
 const user: User = {
