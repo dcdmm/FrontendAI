0d0e23f4b9c7fef2d0abb65bd5344257a4f85b2f
duanmengmeng <dcdmmcomeon@gmail.com>
2026-02-10 16:24:46 +0800

    忧愁

--- study_React/src/App.tsx
+++ study_React/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 import base from './quick_start/a_base'
+import props0 from './quick_start/b_props0'
 import conditionalRendering from './quick_start/b_conditional-rendering'
 import renderingLists from './quick_start/c_rendering_lists'
 import respondingToEvents from './quick_start/d_responding-to-events'
@@ -9,6 +10,7 @@ import hooks from './quick_start/f_hooks'
 
 const demos = [
     { path: '/a', name: '基础', component: base },
+    { path: '/b0', name: 'props学习0', component: props0 },
     { path: '/b', name: '条件渲染', component: conditionalRendering },
     { path: '/c', name: '列表渲染', component: renderingLists },
     { path: '/d', name: '响应事件', component: respondingToEvents },
---
+++ study_React/src/quick_start/b_props0.tsx
@@ -0,0 +1,94 @@
+interface Person {
+    name: string;
+    imageId: string;
+}
+
+interface Detail {
+    age: number;
+    gender: boolean;
+    nation: string;
+}
+
+function getImageUrl(person: Person, size: string = 's') {
+    return (
+        'https://i.imgur.com/' +
+        person.imageId +
+        size +
+        '.jpg'
+    );
+}
+
+function ShowDetail(detail: Detail) {
+    return (
+        <div>
+            <p>Age: {detail.age}</p>
+            <p>Gender: {detail.gender ? 'Male' : 'Female'}</p>
+            <p>Nation: {detail.nation}</p>
+        </div>
+    );
+}
+
+
+// function Avatar(props: { person: Person; size: number; detail: Detail }) {
+//     return (
+//         <>
+//             <img
+//                 className="avatar"
+//                 src={getImageUrl(props.person)}
+//                 alt={props.person.name}
+//                 width={props.size}
+//                 height={props.size}
+//             />
+//             <ShowDetail {...props.detail} />
+//         </>
+//     );
+// }
+
+// 解构对象(与上等价)
+function Avatar({ person, size, detail }: { person: Person; size: number; detail: Detail }) {
+    return (
+        <>
+            <img
+                className="avatar"
+                src={getImageUrl(person)}
+                alt={person.name}
+                width={size}
+                height={size}
+            />
+            {/* 等价于 <ShowDetail age={detail.age} gender={detail.gender} nation={detail.nation} /> */}
+            <ShowDetail {...detail} /> {/* spread (...) syntax */}
+        </>
+    );
+}
+
+
+export default function Profile() {
+    return (
+        <div>
+            <Avatar
+                size={100}
+                person={{
+                    name: 'Katsuko Saruhashi',
+                    imageId: 'YfeOqp2'
+                }}
+                detail={{ age: 80, gender: false, nation: 'Japan' }}
+            />
+            <Avatar
+                size={80}
+                person={{
+                    name: 'Aklilu Lemma',
+                    imageId: 'OKS67lh'
+                }}
+                detail={{ age: 70, gender: true, nation: 'Ethiopia' }}
+            />
+            <Avatar
+                size={50}
+                person={{
+                    name: 'Lin Lanying',
+                    imageId: '1bX5QH6'
+                }}
+                detail={{ age: 75, gender: false, nation: 'China' }}
+            />
+        </div>
+    );
+}
