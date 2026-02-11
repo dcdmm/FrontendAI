53906231dc23ff7ccf325094591e6efdf5cd0cad
duanmengmeng <dcdmmcomeon@gmail.com>
2026-02-11 11:23:08 +0800

    继续

--- study_React/src/App.tsx
+++ study_React/src/App.tsx
@@ -4,8 +4,9 @@ import base from './quick_start/a_base'
 import props0 from './quick_start/b_props0'
 import props1 from './quick_start/b_props1'
 import conditionalRendering from './quick_start/c_conditional-rendering'
-// import renderingLists from './quick_start/c_rendering_lists'
-// import respondingToEvents from './quick_start/d_responding-to-events'
+import renderingLists from './quick_start/c_rendering_lists'
+import respondingToEvents from './quick_start/d_responding-to-events'
+
 // import updatingTheScreen from './quick_start/e_updating_the_screen'
 // import hooks from './quick_start/f_hooks'
 
@@ -13,9 +14,10 @@ const demos = [
     { path: '/a', name: '基础', component: base },
     { path: '/b0', name: 'props学习0', component: props0 },
     { path: '/b1', name: 'props学习1', component: props1 },
-    { path: '/c', name: '条件渲染', component: conditionalRendering },
-    // { path: '/c', name: '列表渲染', component: renderingLists },
-    // { path: '/d', name: '响应事件', component: respondingToEvents },
+    { path: '/c0', name: '条件渲染', component: conditionalRendering },
+    { path: '/c1', name: '列表渲染', component: renderingLists },    
+    { path: '/d', name: '响应事件', component: respondingToEvents },
+    
     // { path: '/e', name: '更新屏幕', component: updatingTheScreen },
     // { path: '/f', name: 'Hooks入门', component: hooks },
 ]
