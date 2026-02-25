c90b4204e94be90f35a8f076e1ed3940108e0bbd
duanmengmeng <dcdmmcomeon@gmail.com>
2026-02-25 17:03:00 +0800

    修复

--- study_React/src/quick_start/i_hooks_useRef0.tsx
+++ study_React/src/quick_start/i_hooks_useRef0.tsx
@@ -1,5 +1,44 @@
-export default function MyApp() {
+import { useRef, useState } from 'react';
+
+// Like state, refs let you retain information between re-renders of a component.
+// Unlike state, setting the ref’s current value does not trigger a re-render.
+
+export default function Counter() {
     return (
-        <></>
+        <>
+            <h2>示例1: 计数器useState实现</h2>
+            <Counter_state />
+            <hr />
+            <h2>示例2: 计数器useRef实现</h2>
+            <Counter_ref />
+        </>
+    );
+}
+
+function Counter_state() {
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1); // 重新渲染组件,并且count的值会更新
+    }
+
+    return (
+        <button onClick={handleClick}>
+            你点击了 {count} 次
+        </button>
+    );
+}
+
+function Counter_ref() {
+    let countRef = useRef(0);
+
+    function handleClick() {
+        countRef.current = countRef.current + 1; // 不会重新渲染组件,但countRef.current的值会更新
+    }
+
+    return (
+        <button onClick={handleClick}>
+            你点击了 {countRef.current} 次
+        </button>
     );
 }
