510ef3f7dd3dfb02aea16ded614114454cdd8708
duanmengmeng <dcdmmcomeon@gmail.com>
2026-02-27 11:50:06 +0800

    over

--- study_React/src/quick_start/i_hooks_useRef1.tsx
+++ study_React/src/quick_start/i_hooks_useRef1.tsx
@@ -1,23 +1,26 @@
 import { useRef } from 'react';
 
+// Refs are an escape hatch. You should only use them when you have to “step outside React”. Common examples of this include managing focus, scroll position, or calling browser APIs that React does not expose.
+// If you stick to non-destructive actions like focusing and scrolling, you shouldn’t encounter any problems. However, if you try to modify the DOM manually, you can risk conflicting with the changes React is making.
 
 function Form0() {
     const inputRef = useRef<HTMLInputElement>(null);
 
     function handleClick() {
         const input = inputRef.current;
-        console.log("input:", input);
+        console.log("DOM:", input);
         if (input) {
-            input.focus();
+            input.focus(); // 获得焦点
+            input.select(); // 选中文本
         }
     }
 
     return (
         <>
             {/* Pass it as <input ref={inputRef}>. This tells React to put this <input>’s DOM node into inputRef.current. */}
-            <input ref={inputRef} />
+            <input ref={inputRef} defaultValue={"dmm"} />
             <button onClick={handleClick}>
-                点击我
+                选中文本
             </button>
         </>
     );
@@ -25,7 +28,7 @@ function Form0() {
 
 
 function MyInput({ ref }: { ref: React.Ref<HTMLInputElement> }) {
-    return <input ref={ref} />;
+    return <input ref={ref} defaultValue={"dc"} />;
 }
 
 function Form1() {
@@ -35,6 +38,7 @@ function Form1() {
         const input = inputRef.current;
         if (input) {
             input.focus();
+            input.select();
         }
     }
 
@@ -42,7 +46,7 @@ function Form1() {
         <>
             <MyInput ref={inputRef} />
             <button onClick={handleClick}>
-                点击我
+                选中文本
             </button>
         </>
     );
