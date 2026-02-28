802095e76bc3f2ec6442645f0436d5ed82304d8b
duanmengmeng <dcdmmcomeon@gmail.com>
2026-02-28 16:58:42 +0800

    迭代

--- study_React/src/hooks/f_useEffect2.tsx
+++ study_React/src/hooks/f_useEffect2.tsx
@@ -1,3 +1,29 @@
+/*
+Effects are an escape hatch from the React paradigm. They let you “step outside” of React and synchronize your components with some external system like a non-React widget, network, or the browser DOM. If there is no external system involved (for example, if you want to update a component’s state when some props or state change), you shouldn’t need an Effect. Removing unnecessary Effects will make your code easier to follow, faster to run, and less error-prone. 
+*/
+
+/*
+function Form() {
+    const [firstName, setFirstName] = useState('Taylor');
+    const [lastName, setLastName] = useState('Swift');
+
+    // 🔴 Avoid: redundant state and unnecessary Effect
+    const [fullName, setFullName] = useState('');
+    useEffect(() => {
+        setFullName(firstName + ' ' + lastName);
+    }, [firstName, lastName]);
+}
+
+function Form() {
+    const [firstName, setFirstName] = useState('Taylor');
+    const [lastName, setLastName] = useState('Swift');
+    // ✅ Good: calculated during rendering
+    const fullName = firstName + ' ' + lastName;
+    // ...
+}
+
+When something can be calculated from the existing props or state, don’t put it in state. Instead, calculate it during rendering.
+*/
 export default function MyApp() {
     return (
         <>
